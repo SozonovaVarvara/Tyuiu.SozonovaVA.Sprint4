3d3cc9aa75054a40976aca91756f115fcf92462f
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-25 14:22:17 +0500

    Add project files.

---
+++ Tyuiu.SozonovaVA.Sprint4.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task0.V22.Lib;
+namespace Tyuiu.SozonovaVA.Sprint4.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            int res = ds.GetSumOddArrEl(numsArray);
+            int wait = 46;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
