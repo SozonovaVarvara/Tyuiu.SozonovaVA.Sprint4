8558ddca289c09601b93ffe242820a4f3c2a1f61
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-25 20:09:24 +0500

    Task6

---
+++ Tyuiu.SozonovaVA.Sprint4.Task6.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task6.V25.Lib;
+namespace Tyuiu.SozonovaVA.Sprint4.Task6.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var ok = new string[] { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+            int res = ds.Calculate(ok);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint4.Task6.V25/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task6.V25.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Созонова В. А. │ АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Класс Array                                                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнила: Созонова Варвара Андреевна | АСОиУб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан строковый массив данных, используя класс Array подсчитайте          *");
+        Console.WriteLine("* количество элементов, длина которых больше 6.                           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string[] array = new string[] { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+        Console.WriteLine("Исходный массив:");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Количество элементов, длина которых больше 6:");
+        int res = ds.Calculate(array);
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+
+    }
+}
