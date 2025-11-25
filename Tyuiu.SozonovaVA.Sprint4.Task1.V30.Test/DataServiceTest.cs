6e124d7d9c6db6c10dea3e46052b9109da8ac131
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-25 14:52:51 +0500

    Task1

---
+++ Tyuiu.SozonovaVA.Sprint4.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task1.V30.Lib;
+namespace Tyuiu.SozonovaVA.Sprint4.Task1.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsarray = { 6, 7, 8, 7, 6, 5, 6, 9, 9, 5, 7, 9, 7, 8, 7 };
+            int res = ds.Calculate(numsarray);
+            int wait = 13824;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint4.Task1.V30/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task1.V30.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях                *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнила: Созонова Варвара Андреевна | АСОиУб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат, и печатает его на экране.                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int len;
+        Console.WriteLine("Введите количество элементов массива:");
+        len = Convert.ToInt32(Console.ReadLine());
+
+        int[] numsarray = new int[len];
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.Write("Введите значение " + i + " элемента массива: ");
+            numsarray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("массив: ");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine(numsarray[i] + "\t");
+
+        }
+        Console.WriteLine();
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(numsarray);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
