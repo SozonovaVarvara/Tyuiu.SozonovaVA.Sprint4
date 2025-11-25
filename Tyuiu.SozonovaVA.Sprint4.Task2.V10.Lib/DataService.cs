a5cb2d451f870d8c9e98c3b7e415d2b357764fd6
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-25 15:22:40 +0500

    Task2

---
+++ Tyuiu.SozonovaVA.Sprint4.Task2.V10.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.SozonovaVA.Sprint4.Task2.V10.Lib
+{
+    public class DataService : ISprint4Task2V10
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 1;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sum *= array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.SozonovaVA.Sprint4.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task2.V10.Lib;
+namespace Tyuiu.SozonovaVA.Sprint4.Task2.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
