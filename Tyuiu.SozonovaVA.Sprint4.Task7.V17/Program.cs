e2991c56b7293680c95baec3b14868a64a7bec37
Варвара Созонова <Tyuiu.SozonovaVA@outlook.com>
2025-11-25 20:42:54 +0500

    Task7

---
+++ Tyuiu.SozonovaVA.Sprint4.Task7.V17/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.SozonovaVA.Sprint4.Task7.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт 4 | Выполнила: Созонова В. А. | АСОиУб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнила: Созонова Варвара Андреевна | АСОиУб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дана строка из одноразрядных цифр \"753159864\". Преобразуйте ее        *");
+        Console.WriteLine("* в матрицу 3 на 3 и подсчитайте количество четных чисел                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int rows = 3;
+        int cols = 3;
+        int[,] mtrx = new int[rows, cols];
+        string str = "753159864";
+        int res = ds.Calculate(rows, cols, str);
+        int index = 0;
+        Console.WriteLine("* матрица                                                                 *");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{str[index]} \t");
+                index++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
